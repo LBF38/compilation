8454d5aa9a6b1d5d8f26b5847a427067c7d6ac5b
LBF38 <contact.lbf38@gmail.com>
2023-04-05 14:12:07 +0200

    style(compiler): :pencil2: typos / formatting

--- TPs/TP5/abstract_syntax.py
+++ TPs/TP5/abstract_syntax.py
@@ -9,7 +9,7 @@ from typing import List
 class AstNode:
     def accept(self, visitor, args=None):
         name = self.__class__.__name__
-        return getattr(visitor, 'visit' + name)(self,args)
+        return getattr(visitor, 'visit' + name)(self, args)
 
 
 class Circuit(AstNode):
